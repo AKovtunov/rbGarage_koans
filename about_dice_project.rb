384ed21fe5f95fe7e1b91a6a9ce055eef90762dd
Alexandr Kovtunov <1sasha.sasha1@gmail.com>
2012-07-21 19:30:45 +0300

    280/280

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :set
+  def roll (num)
+    @set = []
+    (num).times {
+      @set << 1 + rand(6-1)
+    }
+  end
+  def values
+    @set
+  end
+end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -15,10 +15,26 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 class Proxy
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+    @messages.push method_name
+    @object.send method_name, *args
+  end
+
+  def messages
+    @messages
+  end
+
+  def called?(method_name)
+    @messages.include?(method_name)
+  end
+
+  def number_of_times_called(method_name)
+    @messages.select {|x| x==method_name}.length
+  end
+
 end
 
 # The proxy object should pass the following Koan:
